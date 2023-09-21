acdc140f80ba5a9b587e428c9de76a27a589d145
Benjamin Pinet <pinet9u@etu.univ-lorraine.fr>
2023-09-21 12:08:59 +0200

    end

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 #include<iostream>
+class Storey{
+public:
+	Storey();
+	Storey(const Storey& rhs);
+	~Storey();
+	Storey& operator=(const Storey& rhs);
+private:
+	int uid_;
+	static int serial_number_; 
+};
 
 class Building
 {
@@ -8,6 +18,7 @@ public:
 	Building(int identifier);
 	~Building();
 	void print(std::ostream& ou);
+	Building (const Building& rhs);
 	Building& operator=(const Building& rhs);
 private:
 	int id_;
@@ -25,14 +36,18 @@ void Building::print(std::ostream& out){
 	out<<"Building "<<id_<<" at "<<this << std::endl;
 	
 }
+Building::Building(const Building& rhs) : id_(rhs.id_) 
+{
+	std::cout << "Copie de Building" <<  "dans le building" << id_ << std::endl;
+}
 Building& Building::operator=(const Building& rhs){
 	if(this != &rhs ) 
 	{
 	id_=rhs.id_;
 	}
-	std::cout<<"Copy Done"<<std::endl;
 	return *this;
 }
+
 
 
 int main(int argc , char** argv)
@@ -41,22 +56,25 @@ int main(int argc , char** argv)
 	int size{0};
 	std::cin>>size;
 	
-	Building** street = new Building*[size];
+	Building* street = new Building[size];
 	for (int ib=0; ib<size ; ++ib)
 	{
-		street[ib]=new Building(ib)
+		street[ib]= Building(ib);
 	}
-	
+	//Resize array 
+	Building* bigger_street = new Building[size+1];
+	for(int i=0; i<size ; ++i) 
+		bigger_street[i]= street[i];
+	delete[] street; 
+	street=bigger_street;
+	Building city_hall(1000);
+	Building new_city_hall(city_hall); //Test Copy Constructor 
+	street[size++]=new_city_hall;
 	for (int i=0; i<size ; ++i)
 	{
-		if(street[i] != nullptr)
-		{
-			street[i]->print(std::cout);
-		}
-	}
-	for (int i=0; i<size; ++i)
-	{
-		delete street[i];
+
+		street[i].print(std::cout);
+
 	}
 	delete[] street;
 	std::cout << "Done"<<std::endl;
